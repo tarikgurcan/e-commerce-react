9d86dec8c7f3d25e26c1d589471fc9806bda51c7
tarikgurcan <tarikgurcan16@gmail.com>
2021-03-22 21:18:55 +0300

    setting up e-commerce

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import HomePage from "./pages/homepage.component"
+import "./App.css"
 
 function App() {
   return (
     <div>
+      <HomePage/>
     </div>
   );
 }
