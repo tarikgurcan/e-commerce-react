bf83a5695985d9bef7c8ebb394e6bf7de58d6d63
tarikgurcan <tarikgurcan16@gmail.com>
2021-10-22 02:27:20 +0300

    middlewares settings

--- src/components/form-input/form-input.jsx
+++ src/components/form-input/form-input.jsx
@@ -1,16 +1,20 @@
-import React from 'react'
-import "./form-input.styles.scss"
+import React from 'react';
 
-const FormInput = ({handleChange,label,...otherProps}) => {
-    return (
-        <div className="group">
-            <input className="form-input" handlechange={handleChange} {...otherProps}/>
-            {label?(
-            <label className={(`${otherProps.value.lenght?"shrink":""} form-input-label`)}>
-                {label}
-            </label>):null}
-        </div>
-    )
-}
+import './form-input.styles.scss';
 
-export default FormInput
+const FormInput = ({ handleChange, label, ...otherProps }) => (
+  <div className="group">
+    <input className="form-input" onChange={handleChange} {...otherProps} />
+    {label ? (
+      <label
+        className={`${
+          otherProps.value.length ? "shrink" : ""
+        } form-input-label`}
+      >
+        {label}
+      </label>
+    ) : null}
+  </div>
+);
+
+export default FormInput;
